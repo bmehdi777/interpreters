94c3d61b3ff45d57afccf54a7a4eeb9db47ee4dc
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2023-03-27 23:34:00 +0200

    Add test to display program string

---
+++ tests/ast.rs
@@ -0,0 +1,30 @@
+use r_interpreter::lexer::token::{Token, TokenType};
+use r_interpreter::parser::ast::*;
+use r_interpreter::parser::parser::Parser;
+
+#[test]
+fn test_string() {
+    let program: Program = Program {
+        statements: vec![Statement::Let(LetStatement {
+            token: Token {
+                token_type: TokenType::LET,
+                literal: "let".to_owned(),
+            },
+            name: Some(Identifier {
+                token: Token {
+                    token_type: TokenType::IDENT,
+                    literal: "myVar".to_owned(),
+                },
+                value: "myVar".to_owned(),
+            }),
+            value: Some(Expression::Identifier(Identifier {
+                token: Token {
+                    token_type: TokenType::IDENT,
+                    literal: "anotherVar".to_owned(),
+                },
+                value: "anotherVar".to_owned(),
+            })),
+        })],
+    };
+    assert!(format!("{}", program) == "let myVar = anotherVar;")
+}
